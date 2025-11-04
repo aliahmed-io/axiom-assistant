052aca809730a7ecd11e369b2e1404b978cb715b
alinewprogrammer <alihassan182006@gmail.com>
2025-11-05 01:04:12 +0300

    v8

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const dynamic = "force-dynamic";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
-  weight: "100 900",
+  weight: "100 900" ,
 });
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
